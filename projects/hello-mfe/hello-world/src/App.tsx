b87555bcf3e1324ee4822990cb03fe3745f99639
Thomas Lhomme <lhomme.thomas@gmail.com>
2024-03-21 17:27:58 +0530

    Hello MFE, your first MFE folks

---
+++ projects/hello-mfe/hello-world/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import HelloWorld from './HelloWorld/HelloWorld';
+
+function App() {
+  return (
+    <>
+    <h1>Hello World</h1>
+    <HelloWorld></HelloWorld>
+    </>
+  );
+}
+
+export default App;
